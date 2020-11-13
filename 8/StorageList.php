1ac1f12ceae81a1b9e02f992a472ec585e00cc42
Guillaume Astier <astier.gui@gmail.com>
2020-11-13 22:07:27 +0100

    Create StorageList.php

---
+++ 8/StorageList.php
@@ -0,0 +1,76 @@
+<?php
+
+class StorageList
+{
+    /** @param Drupal\block\Entity\Block BLOCK */
+    public const BLOCK = "block";
+
+    /** @param Drupal\editor\Entity\Editor EDITOR */
+    public const EDITOR = "editor";
+
+    /** @param Drupal\field\Entity\FieldConfig FIELD_INSTANCE_CONFIG */
+    public const FIELD_INSTANCE_CONFIG = "field_config";
+
+    /** @param Drupal\field\Entity\FieldStorageConfig FIELD_STORAGE_CONFIG */
+    public const FIELD_STORAGE_CONFIG = "field_storage_config";
+
+    /** @param Drupal\image\Entity\ImageStyle IMAGE_STYLE */
+    public const IMAGE_STYLE = "image_style";
+
+    /** @param Drupal\search\Entity\SearchPage SEARCH_PAGE */
+    public const SEARCH_PAGE = "search_page";
+
+    /** @param Drupal\shortcut\Entity\ShortcutSet SHORTCUT_SET */
+    public const SHORTCUT_SET = "shortcut_set";
+
+    /** @param Drupal\system\Entity\Action ACTION */
+    public const ACTION = "action";
+
+    /** @param Drupal\system\Entity\Menu MENU */
+    public const MENU = "menu";
+
+    /** @param Drupal\user\Entity\Role ROLE */
+    public const ROLE = "use_role";
+
+    /** @param Drupal\views\Entity\View VIEW */
+    public const VIEW = "view";
+
+    /** @param Drupal\block_content\Entity\BlockContent BLOCK_CONTENT */
+    public const BLOCK_CONTENT = "block_content";
+
+    /** @param Drupal\comment\Entity\Comment COMMENT */
+    public const COMMENT = "comment";
+
+    /** @param Drupal\contact\Entity\Message MESSAGE */
+    public const MESSAGE = "contact_message";
+
+    /** @param Drupal\file\Entity\File FILE */
+    public const FILE = "file";
+
+    /** @param Drupal\menu_link_content\Entity\MenuLinkContent MENU_LINK_CONTENT */
+    public const MENU_LINK_CONTENT = "menu_link_content";
+
+    /** @param Drupal\paragraphs\Entity\Paragraph PARAGRAPH */
+    public const PARAGRAPH = "paragraph";
+
+    /** @param Drupal\paragraphs\Entity\ParagraphsType PARAGRAPH_TYPE */
+    public const PARAGRAPH_TYPE = "paragraph_type";
+
+    /** @param Drupal\node\Entity\Node NODE */
+    public const NODE = "node";
+
+    /** @param Drupal\node\Entity\NodeType NODE_TYPE */
+    public const NODE_TYPE = "node_type";
+
+    /** @param Drupal\shortcut\Entity\Shortcut SHORTCUT */
+    public const SHORTCUT = "shortcut";
+
+    /** @param Drupal\user\Entity\User USER */
+    public const USER = "user";
+
+    /** @param Drupal\taxonomy\Entity\Term TERM */
+    public const TERM = "taxonomy_term";
+
+    /** @param Drupal\taxonomy\Entity\Vocabulary VOCABULARY */
+    public const VOCABULARY = "taxonomy_vocabulary";
+}
