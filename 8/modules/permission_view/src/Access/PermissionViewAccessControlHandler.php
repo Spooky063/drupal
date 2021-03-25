36b7f8110dc42988beb9b1ec9fda983e5dd33ef7
Guillaume Astier <astier.gui@gmail.com>
2021-03-25 19:11:03 +0100

    chore: update permission view taxonomy

--- 8/modules/permission_view/src/Access/PermissionViewAccessControlHandler.php
+++ 8/modules/permission_view/src/Access/PermissionViewAccessControlHandler.php
@@ -5,34 +5,34 @@ declare(strict_types=1);
 namespace Drupal\permission_view\Access;
 
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\taxonomy\TermAccessControlHandler;
 
-class PermissionViewAccessControlHandler extends EntityAccessControlHandler
+class PermissionViewAccessControlHandler extends TermAccessControlHandler
 {
     protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account)
     {
+        if ($account->hasPermission('administer taxonomy')) {
+            return AccessResult::allowed()->cachePerPermissions();
+        }
+
         switch ($operation) {
             case 'view':
-                if ($account->hasPermission('administer taxonomy')) {
-                    return AccessResult::allowed();
-                    break;
+                $activeTheme = \Drupal::theme()->getActiveTheme()->getName();
+                $defaultTheme = \Drupal::service('theme_handler')->getDefault();
+
+                if ($activeTheme === $defaultTheme) {
+                    return AccessResult::allowedIfHasPermissions(
+                        $account,
+                        ["permission_view_taxonomy_{$entity->bundle()}"]
+                    );
+                } else {
+                    return parent::checkAccess($entity, $operation, $account);
                 }
 
-                return AccessResult::allowedIfHasPermissions($account, ["permission_view_taxonomy_{$entity->bundle()}", "view {$entity->bundle()} terms"], 'OR');
-                break;
-
-            case 'update':
-                return AccessResult::allowedIfHasPermissions($account, ["edit terms in {$entity->bundle()}", 'administer taxonomy'], 'OR');
-                break;
-
-            case 'delete':
-                return AccessResult::allowedIfHasPermissions($account, ["delete terms in {$entity->bundle()}", 'administer taxonomy'], 'OR');
-                break;
-
             default:
-                return AccessResult::neutral();
+                return parent::checkAccess($entity, $operation, $account);
         }
     }
 }
