b817b3a5cb6d8447b81348d6b350dfbee1752e59
Guillaume Astier <astier.gui@gmail.com>
2024-07-16 10:28:55 +0200

    chore: add new way to instanciate class

--- sandbox/snippets/modules/storage/src/Controller/StorageController.php
+++ sandbox/snippets/modules/storage/src/Controller/StorageController.php
@@ -12,24 +12,29 @@ use Symfony\Component\HttpFoundation\Response;
 
 class StorageController extends ControllerBase implements ContainerInjectionInterface
 {
-    public function __construct(private string $entity_type)
-    {
+    public function __construct(
+      private string $entity_type,
+      protected ArticleStorage $articleStorage,
+    ) {
 
     }
 
     public static function create(ContainerInterface $container): self
     {
       return new self(
-        $container->getParameter('entity_type')
+        $container->getParameter('entity_type'),
+        $container->get('article_storage'),
       );
     }
 
     public function __invoke(): Response
     {
-        /**
-         * @var ArticleStorage $articleStorage
-        */
+        // Two ways to instanciate the class ArticleStorage
+        $articleStorage = $this->articleStorage;
+        /** @var ArticleStorage $articleStorage */
         $articleStorage = $this->entityTypeManager()->getStorage($this->entity_type);
+
+        // Call methods
         $publishedEntities = $articleStorage->getPublishedArticles();
         $countPublishedEntities = $articleStorage->countPublishedArticles();
         $publishedEntitiesWithTags = $articleStorage->getPublishedArticlesWithSpecificTags(['article']);
