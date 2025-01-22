6155264367495805d539f2ea38a058b25e257a6e
Guillaume Astier <astier.gui@gmail.com>
2025-01-22 12:18:26 +0100

    feat(example): module to test ci

---
+++ sandbox/snippets/tests/example/tests/src/Unit/ExampleUnitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\example\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @group example
+ */
+final class ExampleUnitTest extends UnitTestCase
+{
+    public function testUnitFake(): void
+    {
+        $this->assertTrue(TRUE);
+    }
+}
